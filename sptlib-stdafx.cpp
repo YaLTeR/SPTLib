0a21147704bb840e452dd319859ebbd5debb707b
Ivan Molodetskikh <yalterz@gmail.com>
2014-09-02 20:52:23 +0400

    Something bad just happened

---
+++ sptlib-stdafx.cpp
@@ -0,0 +1,1 @@
+#include "sptlib-stdafx.hpp"
---
+++ sptlib.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <codecvt>
+#include <locale>
+#include <string>
+
+#define SPTLIB_VERSION "1.0-beta"
+
+extern std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> string_converter;
+
+extern void ( *EngineConCmd )( const char *cmd );
+extern void ( *EngineGetViewAngles )( float viewangles[3] );
+extern void ( *EngineSetViewAngles )( const float viewangles[3] );
+
+extern void ( *_EngineMsg )( const char *format, ... );
+extern void ( *_EngineDevMsg )( const char *format, ... );
+extern void ( *_EngineWarning )( const char *format, ... );
+extern void ( *_EngineDevWarning )( const char *format, ... );
+
+#ifndef SPT_MESSAGE_PREFIX
+#define SPT_MESSAGE_PREFIX "SPTLib: "
+#endif // SPT_MESSAGE_PREFIX
+
+#define EngineMsg(...)        _EngineMsg(SPT_MESSAGE_PREFIX __VA_ARGS__)
+#define EngineDevMsg(...)     _EngineDevMsg(SPT_MESSAGE_PREFIX __VA_ARGS__)
+#define EngineWarning(...)    _EngineWarning(SPT_MESSAGE_PREFIX __VA_ARGS__)
+#define EngineDevWarning(...) _EngineDevWarning(SPT_MESSAGE_PREFIX __VA_ARGS__)
+
+std::wstring GetFileName( const std::wstring &fileNameWithPath );

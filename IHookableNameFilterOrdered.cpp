73e92714b62f23cbdee3aae1562f109da60cdc0e
Ivan Molodetskikh <yalterz@gmail.com>
2015-01-09 15:36:05 +0300

    Added an ordered name filter interface.

---
+++ IHookableNameFilterOrdered.cpp
@@ -0,0 +1,42 @@
+#include "sptlib-stdafx.hpp"
+
+#include "sptlib.hpp"
+#include "MemUtils.hpp"
+#include "IHookableNameFilterOrdered.hpp"
+
+bool IHookableNameFilterOrdered::CanHook(const std::wstring& moduleFullName)
+{
+	// The closer element is to the start, the more priority it gets.
+	size_t number = 0;
+	bool found = false;
+	for (auto name : m_Names)
+	{
+		if (name == moduleFullName)
+		{
+			found = true;
+			break;
+		}
+		number++;
+	}
+
+	return found && number <= m_HookedNumber;
+}
+
+void IHookableNameFilterOrdered::TryHookAll(bool needToIntercept)
+{
+	size_t number = 0;
+	for (auto name : m_Names)
+	{
+		void* handle;
+		void* start;
+		size_t size;
+		if (MemUtils::GetModuleInfo(name, &handle, &start, &size))
+		{
+			EngineDevMsg("Hooking %s (start: %p; size: %x)...\n", Convert(name).c_str(), start, size);
+			Hook(name, handle, start, size, needToIntercept);
+			m_HookedNumber = number;
+			break;
+		}
+		number++;
+	}
+}
---
+++ IHookableNameFilterOrdered.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cstddef>
+#include <string>
+#include <vector>
+
+#include "IHookableModule.hpp"
+using std::size_t;
+
+class IHookableNameFilterOrdered : public IHookableModule
+{
+public:
+	IHookableNameFilterOrdered(const std::vector<std::wstring>& moduleNames) : m_Names(moduleNames), m_HookedNumber(m_Names.size()) {};
+	virtual bool CanHook(const std::wstring& moduleFullName);
+	virtual void TryHookAll(bool needToIntercept);
+
+protected:
+	std::vector<std::wstring> m_Names;
+	size_t m_HookedNumber;
+};

bf442cbb9459dcabeea2faa526d59f9269487f59
Ivan Molodetskikh <yalterz@gmail.com>
2015-01-26 20:50:28 +0300

    Fixed the ordered name filter.

--- IHookableNameFilterOrdered.cpp
+++ IHookableNameFilterOrdered.cpp
@@ -9,9 +9,10 @@ bool IHookableNameFilterOrdered::CanHook(const std::wstring& moduleFullName)
 	// The closer element is to the start, the more priority it gets.
 	size_t number = 0;
 	bool found = false;
+	auto filename = GetFileName(moduleFullName);
 	for (auto name : m_Names)
 	{
-		if (name == moduleFullName)
+		if (name == filename)
 		{
 			found = true;
 			break;

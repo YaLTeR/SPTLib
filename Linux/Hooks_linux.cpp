8d7581f45c8be24eb6b36b3cf6d4072fa041fda7
Ivan Molodetskikh <yalterz@gmail.com>
2017-03-18 08:30:45 +0300

    Fix warning and indentation

--- Linux/Hooks_linux.cpp
+++ Linux/Hooks_linux.cpp
@@ -41,46 +41,50 @@ namespace Hooks
 			return nullptr;
 
 		// Based on code from Matherunner's TAS Tools v2.0
-	    std::FILE *libfile = std::fopen(p.second.c_str(), "r");
-	    if (!libfile)
-	    {
-	    	perror("Error opening libdl.so");
-	    	return nullptr;
-	    }
+		std::FILE *libfile = std::fopen(p.second.c_str(), "r");
+		if (!libfile)
+		{
+			perror("Error opening libdl.so");
+			return nullptr;
+		}
 
-	    long orig_pos = std::ftell(libfile);
-	    std::fseek(libfile, 0, SEEK_END);
-	    long filesize = std::ftell(libfile);
-	    std::fseek(libfile, orig_pos, SEEK_SET);
+		long orig_pos = std::ftell(libfile);
+		std::fseek(libfile, 0, SEEK_END);
+		size_t filesize = std::ftell(libfile);
+		std::fseek(libfile, orig_pos, SEEK_SET);
 
-	    char *filedat = new char[filesize];
-	    std::fread(filedat, 1, filesize, libfile);
-	    std::fclose(libfile);
+		char *filedat = new char[filesize];
+		if (std::fread(filedat, 1, filesize, libfile) != filesize) {
+			perror("Error reading libdl.so");
+			return nullptr;
+		}
 
-	    Elf32_Ehdr *elf_hdr = (Elf32_Ehdr *)filedat;
-	    Elf32_Shdr *sh_hdr = (Elf32_Shdr *)(filedat + elf_hdr->e_shoff);
-	    Elf32_Shdr *sh_shstrhdr = sh_hdr + elf_hdr->e_shstrndx;
-	    char *sh_shstrtab = filedat + sh_shstrhdr->sh_offset;
+		std::fclose(libfile);
 
-	    int i;
-	    for (i = 0; sh_hdr[i].sh_type != SHT_DYNSYM; i++);
-	    Elf32_Sym *symtab = (Elf32_Sym *)(filedat + sh_hdr[i].sh_offset);
-	    uint64_t st_num_entries = sh_hdr[i].sh_size / sizeof(Elf32_Sym);
+		Elf32_Ehdr *elf_hdr = (Elf32_Ehdr *)filedat;
+		Elf32_Shdr *sh_hdr = (Elf32_Shdr *)(filedat + elf_hdr->e_shoff);
+		Elf32_Shdr *sh_shstrhdr = sh_hdr + elf_hdr->e_shstrndx;
+		char *sh_shstrtab = filedat + sh_shstrhdr->sh_offset;
 
-	    for (i = 0; sh_hdr[i].sh_type != SHT_STRTAB ||
-	             strcmp(sh_shstrtab + sh_hdr[i].sh_name, ".dynstr") != 0; i++);
-	    char *sh_strtab = filedat + sh_hdr[i].sh_offset;
+		int i;
+		for (i = 0; sh_hdr[i].sh_type != SHT_DYNSYM; i++);
+		Elf32_Sym *symtab = (Elf32_Sym *)(filedat + sh_hdr[i].sh_offset);
+		uint64_t st_num_entries = sh_hdr[i].sh_size / sizeof(Elf32_Sym);
+
+		for (i = 0; sh_hdr[i].sh_type != SHT_STRTAB ||
+				 strcmp(sh_shstrtab + sh_hdr[i].sh_name, ".dynstr") != 0; i++);
+		char *sh_strtab = filedat + sh_hdr[i].sh_offset;
 
 		void *result = nullptr;
-	    for (uint64_t i = 0; i < st_num_entries; i++)
-	        if (!strcmp(sh_strtab + symtab[i].st_name, "dlsym"))
-	        {
-	        	result = reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(p.first) + reinterpret_cast<uintptr_t>(symtab[i].st_value));
-	        	break;
-	        }
+		for (uint64_t i = 0; i < st_num_entries; i++)
+			if (!strcmp(sh_strtab + symtab[i].st_name, "dlsym"))
+			{
+				result = reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(p.first) + reinterpret_cast<uintptr_t>(symtab[i].st_value));
+				break;
+			}
 
-	    delete[] filedat;
-	    return result;
+		delete[] filedat;
+		return result;
 	}
 
 	extern "C" void* dlopen(const char* filename, int flag)
